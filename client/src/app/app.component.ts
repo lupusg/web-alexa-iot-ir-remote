cd59fa95dd54a3527571f9b17ed273bf351133f9
lupusg <just_vlad@yahoo.com>
2023-02-27 17:43:09 +0200

    Add signal & pagination interfaces

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
+import { Signal } from './models/signal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  signals: Signal[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Signal[]>>('https://localhost:5001/api/signals?pageSize=30')
+      .subscribe({
+        next: (response: any) => { this.signals = response.data},
+        error: (err) => { console.log(err) },
+        complete: () => { console.log('Completed') }
+      });
+  }
 }
