d521121473d014b2bb7ab573b44691da14b01ac7
lupusg <just_vlad@yahoo.com>
2023-02-28 17:13:14 +0200

    Add manager UI

--- client/src/app/manager/manager.component.ts
+++ client/src/app/manager/manager.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Signal } from '../models/signal';
+import { ManagerService } from './manager.service';
 
 @Component({
   selector: 'app-manager',
   templateUrl: './manager.component.html',
-  styleUrls: ['./manager.component.scss']
+  styleUrls: ['./manager.component.scss'],
 })
-export class ManagerComponent {
+export class ManagerComponent implements OnInit {
+  signals: Signal[] = [];
 
+  constructor (private managerService: ManagerService) {}
+
+  ngOnInit(): void {
+    this.managerService.getSignals().subscribe({
+      next: (response: any) => {
+        this.signals = response.data;
+      }
+    });
+  }
 }
