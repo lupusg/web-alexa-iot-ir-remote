bccee7b4b325364621c614e54aceef2982eac218
lupusg <just_vlad@yahoo.com>
2023-03-01 20:03:53 +0200

    Add paging header shared module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './paging-header/paging-header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagingHeaderComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    PaginationModule.forRoot(),
+  ],
+  exports: [
+    PaginationModule,
+    PagingHeaderComponent
+  ],
 })
 export class SharedModule { }
