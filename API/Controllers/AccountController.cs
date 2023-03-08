2ebaa80ca20bd8000bd1464683ec8c7a296ec404
lupusg <just_vlad@yahoo.com>
2023-03-08 16:18:40 +0200

    Add reponse for exising email on register

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -68,7 +68,7 @@ namespace API.Controllers
 
             var result = await _userManager.UpdateAsync(user);
 
-            if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+            if (result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
 
             return BadRequest("Problem updating the user.");
         }
@@ -95,6 +95,15 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse
+                {
+                    Errors = new[]{ "Email address is in use." }
+                });
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
