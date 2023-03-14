8fc420a14113ebd04bcf19227e14ef6f079d8fb8
lupusg <just_vlad@yahoo.com>
2023-03-14 18:54:44 +0200

    Add signalDto

---
+++ API/Dto/SignalDto.cs
@@ -0,0 +1,10 @@
+namespace API.Dto
+{
+    public class SignalDto
+    {
+        public string Name { get; set; }
+        public string IrData { get; set; }
+        public string AssignedButton { get; set; }
+        public string CreatedAt { get; set; }
+    }
+}
