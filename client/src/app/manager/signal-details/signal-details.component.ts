ed279bd31879fba377586215ae1a06618bc39526
lupusg <just_vlad@yahoo.com>
2023-03-15 18:36:16 +0200

    Add disable form & save, delete buttons if user is not logged in

--- client/src/app/manager/signal-details/signal-details.component.ts
+++ client/src/app/manager/signal-details/signal-details.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
 import { Signal } from 'src/app/shared/models/signal';
 import { ManagerService } from '../manager.service';
 
 @Component({
   selector: 'app-signal-details',
   templateUrl: './signal-details.component.html',
-  styleUrls: ['./signal-details.component.scss']
+  styleUrls: ['./signal-details.component.scss'],
 })
 export class SignalDetailsComponent implements OnInit {
   signal?: Signal;
@@ -18,35 +20,49 @@ export class SignalDetailsComponent implements OnInit {
     createdAt: new FormControl(''),
   });
 
-  constructor(private managerService: ManagerService,
-    private activatedRoute: ActivatedRoute, private router: Router) { }
+  constructor(
+    private managerService: ManagerService,
+    public accountService: AccountService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    if (!this.accountService.isLoggedIn())
+      this.signalForm.disable();
+    
     this.loadSignal();
   }
 
   loadSignal() {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
-    if (id) this.managerService.getSignal(+id).subscribe({
-      next: signal => {
-        this.signalForm.patchValue(signal)
-        this.signal = signal;
-      },
-      error: error => console.log(error)
-    })
+    if (id)
+      this.managerService.getSignal(+id).subscribe({
+        next: (signal) => {
+          this.signalForm.patchValue(signal);
+          this.signal = signal;
+        },
+        error: (error) => console.log(error),
+      });
   }
 
   updateSignal() {
-    this.managerService.editSignal(this.signal?.id, this.signalForm.value).subscribe({
-      next: () => {this.router.navigateByUrl('/manager')},
-      error: error => console.log(error)
-    })
+    this.managerService
+      .editSignal(this.signal?.id, this.signalForm.value)
+      .subscribe({
+        next: () => {
+          this.router.navigateByUrl('/manager');
+        },
+        error: (error) => console.log(error),
+      });
   }
 
   deleteSignal() {
     this.managerService.deleteSignal().subscribe({
-      next: () => {this.router.navigateByUrl('/manager')},
-      error: error => console.log(error)
-    })
+      next: () => {
+        this.router.navigateByUrl('/manager');
+      },
+      error: (error) => console.log(error),
+    });
   }
 }
