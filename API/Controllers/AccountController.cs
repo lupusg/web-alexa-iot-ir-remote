56262b74abc3a39860fbbcf6a702003fb9ac6f32
lupusg <just_vlad@yahoo.com>
2023-03-19 20:50:29 +0200

    Disable register

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -92,35 +92,35 @@ namespace API.Controllers
             };
         }
 
-        [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-        {
+        // [HttpPost("register")]
+        // public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        // {
 
-            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
-            {
-                return new BadRequestObjectResult(new ApiValidationErrorResponse
-                {
-                    Errors = new[]{ "Email address is in use." }
-                });
-            }
+        //     if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+        //     {
+        //         return new BadRequestObjectResult(new ApiValidationErrorResponse
+        //         {
+        //             Errors = new[]{ "Email address is in use." }
+        //         });
+        //     }
 
-            var user = new AppUser
-            {
-                DisplayName = registerDto.DisplayName,
-                Email = registerDto.Email,
-                UserName = registerDto.Email
-            };
+        //     var user = new AppUser
+        //     {
+        //         DisplayName = registerDto.DisplayName,
+        //         Email = registerDto.Email,
+        //         UserName = registerDto.Email
+        //     };
 
-            var result = await _userManager.CreateAsync(user, registerDto.Password);
+        //     var result = await _userManager.CreateAsync(user, registerDto.Password);
 
-            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+        //     if (!result.Succeeded) return BadRequest(new ApiResponse(400));
 
-            return new UserDto
-            {
-                DisplayName = user.DisplayName,
-                Token = _tokenService.CreateToken(user),
-                Email = user.Email
-            };
-        }
+        //     return new UserDto
+        //     {
+        //         DisplayName = user.DisplayName,
+        //         Token = _tokenService.CreateToken(user),
+        //         Email = user.Email
+        //     };
+        // }
     }
 }
